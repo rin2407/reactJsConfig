6aaf066662b7d3e6c1f1bb1ec379b8740b497bd9
rinnv <rinnv@vnext.com.vn>
2021-06-15 20:11:53 +0700

    config i18n and base source

--- configI18n/src/index.tsx
+++ configI18n/src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { I18nextProvider } from 'react-i18next';
-import i18n from './i18n';
+import i18n from './_i18n/i18n';
 ReactDOM.render(
   <React.StrictMode>
     <I18nextProvider i18n={i18n}>
